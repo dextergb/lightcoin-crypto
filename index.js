99d71af73371f1498ef2b21ffeb31d478559f3b7
dextergb <dextergb@rogers.com>
2021-06-04 21:54:22 +0000

    refactor commit

--- index.js
+++ index.js
@@ -13,24 +13,6 @@
 
 let balance = 500.0;
 
-class Transaction {
-  constructor(amount, account) {
-    this.amount = amount;
-    this.account = account;
-  }
-}
-
-class Deposit extends Transaction {
-  commit() {
-    this.account.balance += this.amount;
-  }
-}
-
-class Withdrawal extends Transaction {
-  commit() {
-    this.account.balance -= this.amount;
-  }
-}
 class Account {
   constructor(username) {
     this.username = username;
@@ -39,19 +21,39 @@ class Account {
   }
 }
 
+class Transaction {
+  constructor(amount, account) {
+    this.amount = amount;
+    this.account = account;
+  }
+
+  commit() {
+    this.account.balance += this.value;
+  }
+}
+
+class Deposit extends Transaction {
+  get value() {
+    return this.amount;
+  }
+}
+
+class Withdrawal extends Transaction {
+  get value() {
+    return -this.amount;
+  }
+}
+
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
-const myAccount = new Account("snow-patrol");
-t1 = new Withdrawal(50.25, myAccount);
+const myAccount = new Account("billybob");
+
+console.log("Starting Balance:", myAccount.balance);
+
+const t1 = new Deposit(120.0, myAccount);
 t1.commit();
-console.log("Transaction 1:", t1);
 
-t2 = new Withdrawal(9.99, myAccount);
+const t2 = new Withdrawal(50.0, myAccount);
 t2.commit();
-console.log("Transaction 2:", t2);
 
-t3 = new Deposit(120.0, myAccount);
-t3.commit();
-console.log("Transaction 3:", t3);
-
-console.log("Balance:", balance);
+console.log("Ending Balance:", myAccount.balance);
