6c988007b8f44425322fc7291e05a8fa33df4a7e
dextergb <dextergb@rogers.com>
2021-06-04 21:40:03 +0000

    implemented account and updated depot and withdrawal constructors

--- index.js
+++ index.js
@@ -1,29 +1,59 @@
-let balance = 500.00;
+/*
+* Features:
+
+* Here's a list of features that our code needs to support:
+
+* Allow multiple accounts to be created
+* Each account can have many transactions
+* Allow withdrawals and deposits into accounts
+* Allow us to retrieve the transaction history of an account (all withdrawals and deposits)
+* Allow us to retrieve the current balance of the account at any time
+* Don't allow withdrawals that exceed the remaining balance of the account
+*/
+
+let balance = 500.0;
 
 class Withdrawal {
-
-  constructor(amount) {
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
   }
 
   commit() {
-    balance -= this.amount;
+    this.account.balance -= this.amount;
+  }
+}
+class Deposit {
+  constructor(amount, account) {
+    this.amount = amount;
+    this.account = account;
   }
 
+  commit() {
+    this.account.balance += this.amount;
+  }
 }
-
-
-
+class Account {
+  constructor(username) {
+    this.username = username;
+    // Have the account balance start at $0 since that makes more sense.
+    this.balance = 0;
+  }
+}
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
-
-t1 = new Withdrawal(50.25);
+const myAccount = new Account("snow-patrol");
+t1 = new Withdrawal(50.25, myAccount);
 t1.commit();
-console.log('Transaction 1:', t1);
+console.log("Transaction 1:", t1);
 
-t2 = new Withdrawal(9.99);
+t2 = new Withdrawal(9.99, myAccount);
 t2.commit();
-console.log('Transaction 2:', t2);
+console.log("Transaction 2:", t2);
 
-console.log('Balance:', balance);
+t3 = new Deposit(120.0, myAccount);
+t3.commit();
+console.log("Transaction 3:", t3);
+
+console.log("Balance:", balance);
