ebae583646546fdb047860445be73edb43397b38
dextergb <dextergb@rogers.com>
2021-06-04 21:48:56 +0000

    introduce transaction class to share code between deposit and withdrawal

--- index.js
+++ index.js
@@ -13,24 +13,22 @@
 
 let balance = 500.0;
 
-class Withdrawal {
+class Transaction {
   constructor(amount, account) {
     this.amount = amount;
     this.account = account;
-  }
-
-  commit() {
-    this.account.balance -= this.amount;
   }
 }
-class Deposit {
-  constructor(amount, account) {
-    this.amount = amount;
-    this.account = account;
-  }
 
+class Deposit extends Transaction {
   commit() {
     this.account.balance += this.amount;
+  }
+}
+
+class Withdrawal extends Transaction {
+  commit() {
+    this.account.balance -= this.amount;
   }
 }
 class Account {
